d721f54c03e935b9eafc1849180042c2f69efa71
onePoint <aagvozdkov@yandex.ru>
2024-08-31 09:21:07 +0300

    Обновлен NetworkURL

--- Spar/Network/NetworkURL.swift
+++ Spar/Network/NetworkURL.swift
@@ -13,7 +13,7 @@ enum NetworkURL {
     var url: URL? {
         switch self {
         case .mocProducts:
-            return URL(string: "https://run.mocky.io/v3/656f90f0-54cb-4f02-9e40-75ab31842074")
+            return URL(string: "https://run.mocky.io/v3/a870a382-8f88-4023-b605-b58e4cd851e8")
         }
     }
 }
